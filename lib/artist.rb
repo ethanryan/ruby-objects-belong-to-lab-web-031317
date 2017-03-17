a22d6cf27d0d5d8d8e53ee3a7d319ada2759d728
ethanryan <ethanryan@gmail.com>
2017-03-17 14:17:10 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,10 @@
+class Artist
+  attr_accessor :name # :genre
+
+  # def initialize(name)
+  #   binding.pry
+  #   @name = name
+  #   @genre = genre
+  # end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+class Post
+  attr_accessor :title, :author # :genre
+
+
+  # def initialize(name)
+  #   binding.pry
+  #   @name = name
+  #   @genre = genre
+  # end
+
+end
